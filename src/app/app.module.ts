1f7c3a502c89aa387c90e6e1352c428403b0445b
Siddharth <kambojsiddharth199@gmail.com>
2022-07-08 12:43:19 +0530

    Integrated with Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { ExcelsheetComponent } from './excelsheet/excelsheet.component';
 
@@ -10,7 +10,8 @@ import { ExcelsheetComponent } from './excelsheet/excelsheet.component';
     ExcelsheetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/excelsheet/excelsheet.component.ts
+++ src/app/excelsheet/excelsheet.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
 import * as XLSX from 'xlsx';
 
 @Component({
@@ -7,35 +10,39 @@ import * as XLSX from 'xlsx';
   styleUrls: ['./excelsheet.component.css']
 })
 export class ExcelsheetComponent implements OnInit {
-  
+
   data: [][] | undefined;
-  header : [][] | undefined;
-  constructor() { }
+  header: [][] | undefined;
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
   }
-  onFileChange(evt: any){
-    const target :DataTransfer = <DataTransfer>(evt.target);
+  onFileChange(evt: any) {
+    const target: DataTransfer = <DataTransfer>(evt.target);
 
     // Multiple files error
-    if(target.files.length !== 1){
+    if (target.files.length !== 1) {
       throw new Error("Please upload one file at a time.");
     }
     // Reading File
-    const reader : FileReader = new FileReader();
-    reader.onload = (e:any) => {
-      const bstr : string = e.target.result;
-      const wb : XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
-      
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
       // reading worksheet 
       //  name
-      const wsname : string = wb.SheetNames[0];
+      const wsname: string = wb.SheetNames[0];
       //  content
-      const ws : XLSX.WorkSheet = wb.Sheets[wsname];
-      console.log(ws);
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+      // console.log(ws);
 
       // converting xlsx to json
-      this.data = (XLSX.utils.sheet_to_json(ws, {header : 1}));
+      this.data = (XLSX.utils.sheet_to_json(ws, { header: 1 }));
+
+      // Sending data to Firebase Server
+      this.httpClient.post("https://excelreader-27e76-default-rtdb.firebaseio.com/excelFile/excelData.json", this.data)
+        .subscribe(response => console.log(response))
 
       this.header = this.data[0];
 
@@ -44,5 +51,4 @@ export class ExcelsheetComponent implements OnInit {
 
     reader.readAsBinaryString(target.files[0]);
   }
-
 }
